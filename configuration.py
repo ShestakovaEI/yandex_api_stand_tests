b4d30e7d31a67ff2fa36648aa88e57eb110436d8
Shestakova Elena <shestakovaei@yandex.ru>
2023-05-02 18:11:27 +0600

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://78e34a52-cefb-4439-94b0-c1ea35995d38.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
